b57055eef148dc690a2cb59acaa69d65720e8e06
J30N <171364523+D3vJ30n@users.noreply.github.com>
2025-02-12 09:48:00 +0900

    Initial commit

---
+++ src/main/java/com/j30n/booksumportfolio/BooksumPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.j30n.booksumportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksumPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksumPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/j30n/booksumportfolio/BooksumPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j30n.booksumportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksumPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
